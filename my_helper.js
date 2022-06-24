b27c91cf3fa4fd3c11714b50d91df12435f54ee7
patricia_xiao <patriciaxiao@g.ucla.edu>
2022-06-23 21:47:12 -0400

    updates

--- my_helper.js
+++ my_helper.js
@@ -1,1 +1,2 @@
-var ImageList = ["./img/0.jpeg", "./img/1.jpeg", "./img/2.jpeg", "./img/3.jpeg"]
+var ImageList = ["./img/0.jpeg", "./img/1.jpeg", "./img/2.jpeg", "./img/3.jpeg"];
+var LabelList = ["Here's the overlay text 0", "Here's the overlay text 1", "Here's the overlay text 2", "Here's the overlay text 3"];
